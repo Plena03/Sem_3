23beab8042408bffbecd4ac31204fd48ef695f7b
Elena <p0zhar@yandex.ru>
2022-08-15 15:22:45 +0400

    Задача 23

--- DZ_23/Program.cs
+++ DZ_23/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 23: Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+//3 -> 1, 8, 27
+//5 -> 1, 8, 27, 64, 125
+
+Console.WriteLine("Введите число:");
+string? numberString = Console.ReadLine();
+int x = int.Parse(numberString);
+int index = 1;
+
+if (x <= 0)
+{
+    x = x * (-1);
+}
+while (x >= index)
+{
+    var result = Math.Pow(index, 3); //index * index * index; возведение в куб
+    index++;
+    Console.WriteLine(result);
+}
